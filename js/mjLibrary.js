557a1fdf0712fa9c4588d8970cf53df266383fe0
icatwalker <m804396134@gmail.com>
2017-06-26 08:43:19 +0800

    git 测试1

--- js/mjLibrary.js
+++ js/mjLibrary.js
@@ -50,3 +50,75 @@ m.checkName3=function(){
 };
 Methods.prototype.checkName1();
 
+console.log(Object.prototype);
+//是一个Object 对象的实例（空对象）；
+//Object 和 构造函数都是对象
+
+var o =new Object();//实例化一个新的空对象。
+console.log(o.prototype);// 实例出来的对象没有prototype ，因此不能再实例
+console.log(o.__proto__); //Object;
+
+//F 实力出的对象。
+function F(){}
+var i=new F();
+console.log(i.prototype);//undefined
+console.log(i.__proto__);//Function
+console.log(Function.prototype);// 实际应该是Object  即对象是 Function 实例出来的么
+console.log(Function.__proto__);//
+
+
+
+function F() {};
+F.prototype.a = function() {};
+var c = new F();
+// undefined              F {a: function}
+console.log(i.prototype, c.__proto__);
+
+
+//上述方法 有细微差别。将prototype 对象覆盖 会丢失constrctor
+function M() {}
+M.prototype = {
+    a: function() {}
+};
+var d = new M();
+// undefined             Object {a: function}
+console.log(i.prototype, d.__proto__);
+
+
+
+// 重新指定正确的构造函数
+F.prototype.constructor = F;
+//普通对象
+
+
+// 这是一个函数, 函数是 Function (构造函数)的实例对象, 那么就是由 Function 构造出来的
+Object.__proto__ == Function.prototype // 那么Object的原型, true
+
+
+// 这个是一个普通对象了, 因此属于 Object 的实例
+Function.prototype.__proto__ == Object.prototype // true
+
+function Student(name,age){
+    this.name=name;
+    this.age=age;
+    var no=12234;
+}
+Student.prototype.intr=function(){
+    console.log("我的名字是"+this.name);
+};
+Student.prototype.grade=6;
+var  lilei=new Student("lilei",16);
+console.log(lilei);
+console.log(lilei.grade);
+//console.log(lilei.intr());
+lilei.intr();
+console.log();
+console.log(lilei.__proto__);
+
+var han=new Student("han",15);
+console.log(han.grade);
+console.log(Student.no);
+
+console.log(Student.__proto__==Function.prototype);
+console.log(Function.prototype.__proto__==Object.prototype);
+console.log(Object.prototype.__proto__==null);
